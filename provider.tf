127363ee383630b78228a6e0e1579ca9a5daff5f
Jesus Montoya <60850937+jesmonsa@users.noreply.github.com>
2026-06-03 15:51:48 -0500

    hardening (informe forense): pin provider oci ~> 8.16 y region sin default en el formulario (forzar seleccion explicita)

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">= 5.0.0"
+      version = "~> 8.16" # fijada para evitar deriva de version entre corridas
     }
   }
 
